e6b816b80600657d528d0c678e72924d03717b5a
hnp8uvnb <16102k412345@gmail.com>
2025-12-31 10:31:56 +0700

    ix: Correct path for gitignore

---
+++ Game Project/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    // Chuyển bằng tên của Scene
+    public void ChangeSceneByName(string SampleSence)
+    {
+        SceneManager.LoadScene(SampleSence);
+    }
+
+    // Chuyển bằng chỉ số Index trong Build Settings
+    public void ChangeSceneByIndex(int Begin)
+    {
+        SceneManager.LoadScene(Begin);
+    }
+
+    // Load lại Scene hiện tại (thường dùng khi Game Over)
+    public void ReloadCurrentScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
